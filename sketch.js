31c84eb3e5ac70040f452d61210c9b5dd0680af9
Jake Welch <jaketwelch@gmail.com>
2024-03-27 12:21:14 -0600

    Image raising animation on hover

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 /*
 Developed by Jake Welch
 http://www.jakewelch.design/
-20 February 2024
+Last updated March 27 2024
 
 Austin Aubry photography portfolio, landing page pixelation animation 
 */
+
+///////////////////////////////
+// START OF GLOBAL VARIABLES //
+///////////////////////////////
 
 // Image handling variables
 let imgs = [];
@@ -30,6 +34,10 @@ let res = [];
 let currentResSpeeds = [];
 let maxRes;
 
+// Image raise animation variables
+let imgRaiseAmt = 20; // Amount to raise images by on hover (in pixels)
+let imgRaiseSpeed = 0.8; // Speed of the raise effect (higher value is faster, lower is slower)
+
 // Image links (make sure to keep the order of the photos the same as the order of images.)
 let imageLinks = [
   "https://austinaubry.photo/analog-reflections-in-japan-1",  // 0.jpg link
@@ -46,6 +54,10 @@ let imageLinks = [
   "https://austinaubry.photo/events-1"                        // 11.jpg link
 ];
 let imagePositions = [];
+
+//////////////////////////////
+// END OF GLOBAL VARIABLES //
+/////////////////////////////
 
 // Preload all the images
 function preload() {
@@ -71,6 +83,10 @@ function draw() {
   let xOffset = padding;
   let rowHeights = [];
   let currentRow = 0;
+  let isHoveringOverAnyImage = false; // Track if the cursor is hovering over any image
+
+  // Reset xOffset for drawing logic
+  xOffset = padding;
 
   imgSizes.forEach(({ width, height }, i) => {
     if (i % imgsPerRow === 0 && i !== 0) {
@@ -82,6 +98,8 @@ function draw() {
         rowHeights[currentRow] = height;
       }
     }
+
+    // Adjust xOffset for the next image
     xOffset += width + padding;
   });
 
@@ -91,60 +109,72 @@ function draw() {
   // Adjust yOffset to center the grid vertically
   let yOffset = (windowHeight - totalGridHeight) / 2;
 
+  // Reset xOffset and currentRow for actual drawing
   xOffset = padding;
   currentRow = 0;
   let accumulatedHeight = yOffset;
 
-  // Draw the images with random pixelation speeds
-imgSizes.forEach(({ width, height }, i) => {
-  if (i % imgsPerRow === 0 && i !== 0) {
-    currentRow++;
-    xOffset = padding;
-    accumulatedHeight += rowHeights[currentRow - 1] + padding;
-  }
-
-  if (currentResSpeeds[i] < maxResSpeed) {
-    currentResSpeeds[i] += lowestSpeed;
-  }
-
-  if (res[i] < maxRes) {
-    res[i] += currentResSpeeds[i];
-    if (res[i] / maxRes >= maxResThreshold) {
-      res[i] = maxRes;
+  imgSizes.forEach(({ width, height }, i) => {
+    if (i % imgsPerRow === 0 && i !== 0) {
+      currentRow++;
+      xOffset = padding;
+      accumulatedHeight += rowHeights[currentRow - 1] + padding;
     }
-  }
 
-  let dynamicLayer = createGraphics(Math.ceil(res[i]), Math.ceil((res[i] / imgs[i].width) * imgs[i].height));
-  dynamicLayer.image(imgs[i], 0, 0, dynamicLayer.width, dynamicLayer.height);
-
-  let staticLayer = createGraphics(width, height);
-  staticLayer.clear();
-  staticLayer.noSmooth();
-  staticLayer.image(dynamicLayer, 0, 0, width, height);
-
-  image(staticLayer, xOffset, accumulatedHeight);
-
-  imagePositions[i] = { x: xOffset, y: accumulatedHeight, width, height, link: imageLinks[i] };
-
-  xOffset += width + padding;
-
-  dynamicLayer.remove();
-  staticLayer.remove();
-});
-
-  // Hand cursor on hover logic (moved outside the forEach loop for efficiency)
-  let isHovering = false;
-  for (let { x, y, width, height } of imagePositions) {
-    if (mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height) {
-      cursor(HAND);
-      isHovering = true;
-      break;
+    // Update pixelation animation logic here
+    if (currentResSpeeds[i] < maxResSpeed) {
+      currentResSpeeds[i] += lowestSpeed;
     }
-  }
-  if (!isHovering) {
+
+    if (res[i] < maxRes) {
+      res[i] += currentResSpeeds[i];
+      if (res[i] / maxRes >= maxResThreshold) {
+        res[i] = maxRes;
+      }
+    }
+
+    let dynamicLayer = createGraphics(Math.ceil(res[i]), Math.ceil((res[i] / imgs[i].width) * imgs[i].height));
+    dynamicLayer.image(imgs[i], 0, 0, dynamicLayer.width, dynamicLayer.height);
+
+    let staticLayer = createGraphics(width, height);
+    staticLayer.clear();
+    staticLayer.noSmooth();
+    staticLayer.image(dynamicLayer, 0, 0, width, height);
+
+    // Hover effect logic (as previously described)
+    let isHovering = mouseX >= imagePositions[i].x && mouseX <= imagePositions[i].x + width && 
+                     mouseY >= imagePositions[i].y && mouseY <= imagePositions[i].y + height && 
+                     windowWidth >= 860; // Applies only for desktop
+
+    if (isHovering) {
+      isHoveringOverAnyImage = true;
+      if (imagePositions[i].hoverOffset === undefined) imagePositions[i].hoverOffset = 0;
+      imagePositions[i].hoverOffset = lerp(imagePositions[i].hoverOffset, imgRaiseAmt, imgRaiseSpeed);
+    } else {
+      if (imagePositions[i].hoverOffset === undefined) imagePositions[i].hoverOffset = 0;
+      imagePositions[i].hoverOffset = lerp(imagePositions[i].hoverOffset, 0, imgRaiseSpeed);
+    }
+
+    let adjustedYOffset = accumulatedHeight - (imagePositions[i].hoverOffset || 0);
+
+    // Draw the image with adjusted yOffset
+    image(staticLayer, xOffset, adjustedYOffset);
+
+    xOffset += width + padding;
+
+    dynamicLayer.remove();
+    staticLayer.remove();
+  });
+
+  // Update cursor based on hovering state
+  if (isHoveringOverAnyImage) {
+    cursor(HAND);
+  } else {
     cursor(ARROW);
   }
 }
+
+
 
 // Window resizing function
 function windowResized() {
